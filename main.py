c6cb8fe2ed8cdb8336b5a711fc790e40929ff9ab
RasaStulge <rasakunigiskyte@yahoo.com>
2021-12-02 20:40:46 +0200

    TASK 5

---
+++ main.py
@@ -0,0 +1,15 @@
+# Create a function that will calculate the number of upper and lower case letters in a string
+# eg for: "The quick Brown Fox"
+# Expected result:
+# Number of uppercase letters: 3, number of lowercase letters: 13
+# Hint: use a loop, conditional statement and appropriate methods for the string
+
+s=input('Enter your text:')
+u=0
+l=0
+for i in s:
+    if i.isupper():
+        u=u+1
+    elif i.islower():
+        l=l+1
+print('The number of upper case letters {} and the number of lower case letters {}'.format(u,l))
